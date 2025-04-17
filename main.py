7b494c4a2f9b75121605a326ed3f919269c8c0cb
Nuno Bispo <nuno.m.bispo@gmail.com>
2025-04-17 09:38:50 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, FastAPI!"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "query": q}
+
+
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: bool = None
+
+@app.post("/items/")
+def create_item(item: Item):
+    return {"item_name": item.name, "item_price": item.price, "offer": item.is_offer}
+
